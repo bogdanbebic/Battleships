1592fe06f01885e2014609ef5575e3b58a87393a
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2020-04-23 19:18:31 +0200

    Added welcome page border highlighting on incorrect input, localStorage

--- battleship-welcome.js
+++ battleship-welcome.js
@@ -3,8 +3,27 @@ $(document).ready(
         let regex = /^[a-zA-Z0-9_]{3,15}$/
         let username1 = $("#username1").val();
         let username2 = $("#username2").val();
+
+        if (!regex.test(username1)) {
+            $("#username1").addClass("border border-danger");
+        }
+        else {
+            $("#username1").removeClass("border border-danger");
+        }
+
+        if (!regex.test(username2)) {
+            $("#username2").addClass("border border-danger");
+        }
+        else {
+            $("#username2").removeClass("border border-danger");
+        }
+
         if (!regex.test(username1) || !regex.test(username2)) {
             event.preventDefault();
+        }
+        else {
+            localStorage.setItem("username1", username1);
+            localStorage.setItem("username2", username2);
         }
     })
 );
