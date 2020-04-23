771bb1a351ee54d7e6c6e01b5d732570b3dc2538
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2020-04-23 23:04:19 +0200

    Added text set "X" for clicked td in attackable table in game page

--- battleship-game.js
+++ battleship-game.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
-    alert("Hello, World!");
+    $(".attackable td").click(function (event) {
+        $(this).text("X");
+
+        // $(".attackable td").index(this) -> returns linearized index of td in attackable table
+        alert($(".attackable td").index(this));
+    })
+
 });
