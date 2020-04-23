b63529a2f4d41fac1ac2fa7c97b076679c074eeb
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2020-04-23 22:36:15 +0200

    Added focus to incorrect username input field

--- battleship-welcome.js
+++ battleship-welcome.js
@@ -4,18 +4,19 @@ $(document).ready(
         let username1 = $("#username1").val();
         let username2 = $("#username2").val();
 
-        if (!regex.test(username1)) {
-            $("#username1").addClass("border border-danger");
-        }
-        else {
-            $("#username1").removeClass("border border-danger");
-        }
-
         if (!regex.test(username2)) {
-            $("#username2").addClass("border border-danger");
+            $("#username2").addClass("border border-danger").focus();
+
         }
         else {
             $("#username2").removeClass("border border-danger");
+        }
+
+        if (!regex.test(username1)) {
+            $("#username1").addClass("border border-danger").focus();
+        }
+        else {
+            $("#username1").removeClass("border border-danger");
         }
 
         if (!regex.test(username1) || !regex.test(username2)) {
