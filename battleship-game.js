3f57dfe22bd8b4552a451f31b62bc38873abf479
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2020-04-25 18:09:53 +0200

    Added game params loading from localStorage to game page

--- battleship-game.js
+++ battleship-game.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
+    let username1 = localStorage.getItem("username1");
+    let username2 = localStorage.getItem("username2");
+
+    let tablePlayer1 = JSON.parse(localStorage.getItem("battleshipsPlayer1"));
+    let tablePlayer2 = JSON.parse(localStorage.getItem("battleshipsPlayer2"));
+
     $(".attackable td").click(function (event) {
         $(this).text("X");
 
         // $(".attackable td").index(this) -> returns linearized index of td in attackable table
         alert($(".attackable td").index(this));
-    })
+    });
 
 });
